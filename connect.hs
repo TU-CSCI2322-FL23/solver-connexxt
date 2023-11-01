4db8dc4e3bb31dfae1203401f1b088e86e902910
dsuperal <dsuperal@trinity.edu>
2023-11-01 13:11:52 -0500

    ignore

--- connect.hs
+++ connect.hs
@@ -5,17 +5,25 @@ import Data.Ratio ((%))
 import Data.Tuple (swap)
 import Debug.Trace
 
-data Color = Red | Black 
+data Color = Red | Black
+
 type Player = (String, Color) -- instance of the person playing (Name, Red or Black)
-type Board = [[Maybe Color]]-- write a list of lists laterrr
+
+type Board = [[Maybe Color]] -- write a list of lists laterrr
+
 type Game = (Board, Color)
+
 type Move = Int -- what's the index into the column?
 -- in connect 4, you can only choose the x coordinate
---so you can only look @ which List you're affecting 
+-- so you can only look @ which List you're affecting
 
---whosTurn :: Player -> Bool
---whosTurn person = 
+-- whosTurn :: Player -> Bool
+-- whosTurn person =
 
---MAKE FUNCTION TO CATCH USER ERRORS FOR MAKING PLAYERS (MORE THAN 1 OR LESS THAN 2)
+-- MAKE FUNCTION TO CATCH USER ERRORS FOR MAKING PLAYERS (MORE THAN 1 OR LESS THAN 2)
 
---type Game = Location | Turn 
+-- type Game = Location | Turn
+
+CheckMove :: Move -> Board -> Bool 
+
+MakeMove :: -> [Move] --Game 
