22594c79a05fbd7b4bcdcac73241182daa7ce49c
Lwargomake <lwargo@trinity.edu>
2023-11-01 13:03:51 -0500

    changed winner to data

--- connect.hs
+++ connect.hs
@@ -7,15 +7,13 @@ import Debug.Trace
 
 data Color = Red | Black 
 type Player = (String, Color) -- instance of the person playing (Name, Red or Black)
+data Winner = Tie | Red | Black | Null 
+--type Winner = Maybe Color -- red, black, or null
 type Board = [[Maybe Color]]-- write a list of lists laterrr
-type Game = (Board, Color)
+type Game = (Board, Color, Winner)
 type Move = Int -- what's the index into the column?
 -- in connect 4, you can only choose the x coordinate
 --so you can only look @ which List you're affecting 
 
---whosTurn :: Player -> Bool
---whosTurn person = 
 
 --MAKE FUNCTION TO CATCH USER ERRORS FOR MAKING PLAYERS (MORE THAN 1 OR LESS THAN 2)
-
---type Game = Location | Turn 
