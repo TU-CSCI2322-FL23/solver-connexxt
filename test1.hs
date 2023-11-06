989ef63a8e11087132e43f07265e3a8aed7d4532
dsuperal <dsuperal@trinity.edu>
2023-11-06 13:13:06 -0600

    11/06

--- test1.hs
+++ test1.hs
@@ -1,13 +1,32 @@
 module TestCases where
-import connect
+import Connect
+--module Testing where
+--import Scavenge
+import Data.Semigroup
+--import Dictionaries
+import Control.Monad.Extra
+import Test.Grader.Core
+import Test.Grader.Rubric
+import Test.Grader.Eval
+import Test.Grader.Tests
+import Test.Grader.Parsing.Util
+import Test.Grader.Parsing
+import Data.List
+import Control.Monad.Trans.RWS
 
 player1 = (Fred, Red)
 player2=(James, Black)
 
-testValidMoves :: Grader String
+testValidMoves :: Grader Int
 testValidMoves = assess "validMoves" 4 $ do
-    let insertColor1 = --int
-
+    let insertColor1 = 4
+        insertColor2 = 4
+        insertColor3 = 4
+        moves1 = 4
+        moves2 = 4
+        moves3 = 4 -- moves is it showing how it is a connect 4 and shows how it is not a connect 4 
+    check "that validMoves are valid lol" $ 
+    --check that it makes a connect 4 and when it does not make a connect 4
 
 
 
@@ -17,10 +36,10 @@ testValidMoves = assess "validMoves" 4 $ do
         --moves1 = ["DIE","DO","I","ME","MOVE","MOVIE","PM"]
        -- moves2 = ["FEW","I","IF","KEY","OF","OFF","OFFICE","OK","WE","WIFE"]
         --moves3 = ["A","GUN","LAW","RUN","WAR"]
-    check "that validMoves the valid moves for \"PEMDOVZIJM\" with shortDict" $ (sort $ validMoves shortDict hand1) `shouldBe` moves1
-    check "that validMoves finds the valid moves for \"EIFYOZWFKC\" with shortDict" $ (sort $ validMoves shortDict hand2) `shouldBe` moves2
-    check "that validMoves finds the valid moves for \"AAJNURWLGG\" with shortDict" $ (sort $ validMoves shortDict hand3) `shouldBe` moves3
-    check "that validMoves finds the valid moves for the empty list" $ (sort $ validMoves shortDict "") `shouldBe` []
+    --check "that validMoves the valid moves for \"PEMDOVZIJM\" with shortDict" $ (sort $ validMoves shortDict hand1) `shouldBe` moves1
+    --check "that validMoves finds the valid moves for \"EIFYOZWFKC\" with shortDict" $ (sort $ validMoves shortDict hand2) `shouldBe` moves2
+    --check "that validMoves finds the valid moves for \"AAJNURWLGG\" with shortDict" $ (sort $ validMoves shortDict hand3) `shouldBe` moves3
+    --check "that validMoves finds the valid moves for the empty list" $ (sort $ validMoves shortDict "") `shouldBe` []
 
 tree :: Grader String
 tree = describe "connexxt" $ do
