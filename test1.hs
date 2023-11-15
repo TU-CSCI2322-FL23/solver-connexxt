b84632413b3a50dc720c89db3057551a722b7659
dsuperal <dsuperal@trinity.edu>
2023-11-15 12:16:14 -0600

    nov 15

--- test1.hs
+++ test1.hs
@@ -1,47 +1,49 @@
 module TestCases where
 
 import Connect
-import hspec
 
-player1 = (Fred, Red)
+-- import Test.Hspec
 
-player2 = (James, Black)
+-- player1 = (Fred, Red)
 
-main :: IO ()
-main = hspec $ do
-  describe "checkWin" $ do
-    it "returns Just Red when there is a vertical win for Red" $
-      checkWin
-        [ [Just Red, Just Red, Just Red, Just Red],
-          [Nothing, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Nothing]
-        ]
-        `shouldBe` Just Red
+-- player2 = (James, Black)
 
-    it "returns Just Black when there is a horizontal win for Black" $
-      checkWin
-        [ [Nothing, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Nothing],
-          [Just Black, Just Black, Just Black, Just Black],
-          [Nothing, Nothing, Nothing, Nothing]
-        ]
-        `shouldBe` Just Black
+-- main :: IO ()
+-- main = hspec $ do
+makeMove :: Grader String
+makeMove = assess "checkWin" $ do
+  it "returns Just Red when there is a vertical win for Red" $
+    checkWin
+      [ [Just Red, Just Red, Just Red, Just Red],
+        [Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing]
+      ]
+      `shouldBe` Just Red
 
-    it "returns Just Red when there is a diagonal win for Red" $
-      checkWin
-        [ [Just Red, Nothing, Nothing, Nothing],
-          [Nothing, Just Red, Nothing, Nothing],
-          [Nothing, Nothing, Just Red, Nothing],
-          [Nothing, Nothing, Nothing, Just Red]
-        ]
-        `shouldBe` Just Red
+  it "returns Just Black when there is a horizontal win for Black" $
+    checkWin
+      [ [Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing],
+        [Just Black, Just Black, Just Black, Just Black],
+        [Nothing, Nothing, Nothing, Nothing]
+      ]
+      `shouldBe` Just Black
 
-    it "returns Nothing when there is no winner" $
-      checkWin
-        [ [Just Red, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Nothing],
-          [Nothing, Nothing, Nothing, Just Black]
-        ]
-        `shouldBe` Nothing
+  it "returns Just Red when there is a diagonal win for Red" $
+    checkWin
+      [ [Just Red, Nothing, Nothing, Nothing],
+        [Nothing, Just Red, Nothing, Nothing],
+        [Nothing, Nothing, Just Red, Nothing],
+        [Nothing, Nothing, Nothing, Just Red]
+      ]
+      `shouldBe` Just Red
+
+  it "returns Nothing when there is no winner" $
+    checkWin
+      [ [Just Red, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Nothing],
+        [Nothing, Nothing, Nothing, Just Black]
+      ]
+      `shouldBe` Nothing
